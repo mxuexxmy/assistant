e7a6de1e03fcf7feee96bd46555144479c50643f
mxuexxmy <1989718921@qq.com>
2021-04-07 17:08:07 +0800

    Basic completion of the management side

--- src/main/java/xyz/mxue/assistant/controller/UserController.java
+++ src/main/java/xyz/mxue/assistant/controller/UserController.java
@@ -4,12 +4,14 @@ package xyz.mxue.assistant.controller;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import xyz.mxue.assistant.commons.enumeration.UserStatusEnum;
 import xyz.mxue.assistant.entity.User;
 import xyz.mxue.assistant.model.PageResult;
 import xyz.mxue.assistant.model.Result;
@@ -17,7 +19,9 @@ import xyz.mxue.assistant.service.UserService;
 
 import javax.annotation.Resource;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -93,9 +97,23 @@ public class UserController {
     }
 
     /**
+     * 修改个人信息-保存
+     *
+     * @param user 用户信息
+     * @return Result<String>
+     */
+    @PostMapping("/update-user-info")
+    @ResponseBody
+    public Result<String> updateUserInfo(User user) {
+        System.out.println("user:" + user);
+        boolean b = userService.updateById(user);
+        return b == true ? Result.succeed("修改成功！") : Result.failed("修改失败！");
+    }
+
+    /**
      * 用户注册
      *
-     * @param user 用户学习
+     * @param user 用户信息
      * @return Result<String>
      */
     @PostMapping("/register")
@@ -110,10 +128,159 @@ public class UserController {
      * @return String
      */
     @GetMapping("/password")
-    public String updatePassword(ModelMap map) {
+    public String password(ModelMap map) {
         User user = userService.getById((Serializable) StpUtil.getLoginId());
         map.put("user", user);
         return prefix + "/member/user-pwd";
+    }
+
+    /**
+     *  添加用户页面
+     *
+     * @return String
+     */
+    @GetMapping("/add-user-page")
+    public String addUserPage() {
+        return prefix + "/user/add";
+    }
+
+    /**
+     * 保存用户信息
+     * @param user 用户信息
+     * @return Result<String>
+     */
+    @PostMapping("/save-user-info")
+    @ResponseBody
+    public Result<String> savaUserInfo(User user) {
+        user.setStatus(UserStatusEnum.NORMAL.getValue());
+        user.setPassword(SecureUtil.md5(user.getPassword()));
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        boolean save = userService.save(user);
+        return save == true ? Result.succeed("添加用户成功！") : Result.failed("添加用户失败!");
+    }
+
+    /**
+     * 修改用户信息页面
+     *
+     * @param id 用户id
+     * @return String
+     */
+    @GetMapping("/update-page/{id}")
+    public String editUserInfo(@PathVariable Long id, ModelMap map) {
+        map.put("userInfo", userService.getById(id));
+        return prefix + "/user/update";
+    }
+
+    /**
+     * 保存修改的用户信息
+     *
+     * @param user 用户信息
+     * @return Result<String>
+     */
+    @PostMapping("/save-user-edit")
+    @ResponseBody
+    public Result<String> saveUserEdit(User user) {
+        User originUserInfo = userService.getById(user.getId());
+        if (!originUserInfo.getPassword().equals(user.getPassword())) {
+            user.setPassword(SecureUtil.md5(user.getPassword()));
+        }
+        user.setUpdateTime(new Date());
+        boolean b = userService.updateById(user);
+        return b == true ? Result.succeed("用户信息修改成功！") : Result.failed("用户信息修改失败！");
+    }
+
+    /**
+     * 通过 ID 删除用户
+     *
+     * @param id 用户id
+     * @return Result<String>
+     */
+    @GetMapping("/delete/{id}")
+    @ResponseBody
+    public Result<String> deleteUserById(@PathVariable String id) {
+        User user = userService.getById(id);
+        user.setStatus(UserStatusEnum.DELETE.getValue());
+        boolean b = userService.updateById(user);
+        return b == true ? Result.succeed("用户删除成功！") : Result.failed("用户已删除！");
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param idsStr id集合
+     * @return Result<String>
+     */
+    @PostMapping("/batch-delete")
+    @ResponseBody
+    public Result<String> deleteUserByIds(String idsStr) {
+        List<User> userList = userService.listByIds(Arrays.asList(idsStr.split(",")));
+        for (User user : userList) {
+            user.setStatus(UserStatusEnum.DELETE.getValue());
+        }
+        boolean b = userService.updateBatchById(userList);
+        return b == true ? Result.succeed("批量删除成功！") : Result.failed("批量删除失败！");
+    }
+
+    /**
+     * 批量正常
+     *
+     * @param idsStr id集合
+     * @return Result<String>
+     */
+    @PostMapping("/batch-normal")
+    @ResponseBody
+    public Result<String> batchUserNormalByIds(String idsStr) {
+        List<User> userList = userService.listByIds(Arrays.asList(idsStr.split(",")));
+        for (User user : userList) {
+            user.setStatus(UserStatusEnum.NORMAL.getValue());
+        }
+        boolean b = userService.updateBatchById(userList);
+        return b == true ? Result.succeed("批量正常成功！") : Result.failed("批量正常失败！");
+
+    }
+
+    /**
+     * 批量 冻结
+     *
+     * @param idsStr id集合
+     * @return Result<String>
+     */
+    @PostMapping("/batch-freeze")
+    @ResponseBody
+    public Result<String> batchUserFreezeByIds(String idsStr) {
+        List<User> userList = userService.listByIds(Arrays.asList(idsStr.split(",")));
+        for (User user : userList) {
+            user.setStatus(UserStatusEnum.FREEZE.getValue());
+        }
+        boolean b = userService.updateBatchById(userList);
+        return b == true ? Result.succeed("批量冻结成功！") : Result.failed("批量冻结失败！");
+    }
+
+
+    /**
+     * 修改密码保存
+     *
+     * @return Result<String>
+     */
+    @PostMapping("/update-password")
+    @ResponseBody
+    public Result<String> updatePassword(Long id, String oldPwd, String pwd) {
+        User user = userService.getById(id);
+        if (user == null) {
+            return Result.failed("用户不存在!");
+        }
+        // 判断旧密码
+        if (!user.getPassword().equals(SecureUtil.md5(oldPwd))) {
+            return Result.failed("输入的旧密码错误！");
+        }
+        // 修改密码
+        user.setPassword(SecureUtil.md5(pwd));
+        boolean b = userService.updateById(user);
+        if (b) {
+            StpUtil.logout();
+        }
+        return b == true ? Result.succeed("密码修改成功！") : Result.failed("密码修改失败！");
     }
 
     /**
